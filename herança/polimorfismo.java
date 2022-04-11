c2eabacf1f00b7576de25d15cdd168b2e3965d8b
WesleyCarlos41 <wely_42@hotmail.com>
2022-04-11 16:41:38 -0300

    Aula11/04

---
+++ herança/polimorfismo.java
@@ -0,0 +1,15 @@
+package herança;
+
+public class polimorfismo {
+
+	public static void main(String[] args) {
+		cachorro toto = new cachorro ();
+		cavalo pocoto = new cavalo ();
+		preguica pre = new preguica();
+		
+		toto.som();
+		pocoto.som();
+		pre.som();
+	}
+
+}
